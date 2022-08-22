17783b094e9324e47125e28d3f10b48317646e65
YohanAstier <91124079+YohanAstier@users.noreply.github.com>
2022-08-22 12:12:38 +0200

    fix de agenda

--- app/src/main/java/com/example/eglisenoisy/ui/agenda/AgendaFragment.java
+++ app/src/main/java/com/example/eglisenoisy/ui/agenda/AgendaFragment.java
@@ -11,16 +11,22 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.eglisenoisy.R;
+import com.example.eglisenoisy.databinding.FragmentAgendaBinding;
 import com.example.eglisenoisy.databinding.FragmentHomeBinding;
 import com.example.eglisenoisy.ui.home.HomeViewModel;
 
 public class AgendaFragment extends Fragment {
+    private FragmentAgendaBinding binding;
+    private AgendaViewModel agendaViewModel;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         AgendaViewModel agendaViewModel =
                 new ViewModelProvider(this).get(AgendaViewModel.class);
 
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        binding = FragmentAgendaBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
         return root;
     }
 
